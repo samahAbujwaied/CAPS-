4448232eb28c201535e25a04762e99361829f42c
samah-abujwaied <samah.abujweed@yahoo.com>
2021-08-23 21:18:00 +0300

    lab12

---
+++ vendor/vendor.js
@@ -0,0 +1,13 @@
+'use strict';
+require('dotenv').config();
+
+const faker = require('faker');
+
+const io = require('socket.io-client');
+let host = 'http://localhost:8080';
+const pickup = io.connect(host);
+
+pickup.on('order', payload => {
+    console.log('Thank you for delivering', payload)
+});
+
